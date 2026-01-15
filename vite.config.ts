6c7e9584766203aa0165eb96073d228fa85755d8
momentumcoachescontent-wq <momentumcoaches.content@gmail.com>
2026-01-15 17:50:17 -0600

    Fix import statements in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import build from '@hono/vite-build/cloudflare-pages'
 import devServer from '@hono/vite-dev-server'
 import adapter from '@hono/vite-dev-server/cloudflare'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   // Force Vite to output into /build instead of /dist
