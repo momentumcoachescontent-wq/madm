ceb1cbf5da0d2a3c104f5f91bfcaa790a64643a4
momentumcoachescontent-wq <momentumcoaches.content@gmail.com>
2026-01-19 10:33:20 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   // Force Vite to output into /build instead of /dist
   build: {
-    outDir: "build",
+    outDir: "dist",
     emptyOutDir: true,
   },
   plugins: [react()],
